9b3125c0705602cf3f33582d6ce74baf25a51e9c
samandar-dev <samandarbohodirov01@gmail.com>
2022-03-04 09:06:36 +0500

    adds

---
+++ main.js
@@ -0,0 +1,171 @@
+// 1 masala ==================================
+
+// let a = 16;
+// let b = 17;
+// let c = 19;
+
+// if (a >= b && a >= c) {
+//   console.log("A katta");
+// } else if (b >= a && b >= c) {
+//   console.log("B katta");
+// } else if (c >= a && c >= b) {
+//   console.log("C katta");
+// } else if (a == b && b == c) {
+//   console.log("Hammasi Teng");
+// } else {
+//   console.log("????????");
+// }
+
+
+
+// 2 masala ==================================
+
+// let a = 3;
+// let b = 9;
+// let c = 10;
+// let ab = a + b;
+
+// if (ab > c) {
+//   console.log("YES");
+// } else {
+//   console.log("NO");
+// }
+
+
+
+// 3 masala ==================================
+
+// let a = 6;
+// let b = 6;
+// let c = 15;
+
+// if (a == b && b == c) {
+//   console.log("Teng tomonli");
+// } else if (a == c || b == c || a == b) {
+//   console.log("Teng yonli");
+// } else {
+//   console.log("Turli tomonli");
+// }
+
+
+
+// 4 masala ==================================
+
+// let a = 3;
+// let b = 5;
+// let c = 4;
+
+// if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) {
+//   console.log("YES");
+// } else if (Math.pow(a, 2) + Math.pow(c, 2) == Math.pow(b, 2)) {
+//   console.log("YES");
+// } else if (Math.pow(b, 2) + Math.pow(c, 2) == Math.pow(a, 2)) {
+//   console.log("YES");
+// } else {
+//   console.log("NO");
+// }
+
+
+
+// 5 masala ==================================
+
+// let n = +prompt("1 dan 5 gacha bolgan son kiriting -->> ");
+
+// if (n == 1) {
+//   console.log("Bosh barmoq");
+// } else if (n == 2) {
+//   console.log("Korsatkich barmoq");
+// } else if (n == 3) {
+//   console.log("O'rta barmoq");
+// } else if (n == 4) {
+//   console.log("Nomsiz barmoq");
+// } else if (n == 5) {
+//   console.log("Kichik barmoq");
+// } else {
+//   console.log("Noto'g'ri malumot kiritildi");
+// }
+
+
+
+// 6 masala ?????????????????????????????
+
+// let n = 126;
+// let natija = 0;
+
+// for (let i = n; i > 0; i--) {
+//   natija += i % 10
+// }
+// console.log(`Yig'indi ->> ${natija}`);
+
+
+
+// 7 masala =================================
+
+// let n = 10;
+// let natija = 0;
+
+// for (let i = 0; i <= n; i++) {
+//   natija += i;
+// }
+// console.log(`Natija ->> ${natija}`);
+
+
+
+// 8 masala ================================
+
+// let n = 10;
+// let natija = 0;
+
+// for (let i = 0; i <= n; i++) {
+//   if (i % 2 == 0)
+//     natija += i;
+// }
+// console.log(`Natija ->> ${natija}`);
+
+
+
+// 9 masala ??????????????????????????????????
+
+// let n = 10;
+// let natija = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   natija += i
+//   natija *= i - i
+
+//   console.log(natija);
+// }
+
+
+
+// 10 masala ??????????????????????????????????
+
+// let n = 121;
+// let nn = 0;
+// let natija1 = 0;
+// let natija2 = 0;
+
+// for (let i = 0; i <= n; i++) {
+//   console.log(i);
+//   if (i % 10 == 1) {
+//     natija1 += 1
+//   } else if (i == 2) {
+//     natija2 += 1
+//   }
+// }
+
+// console.log(natija1, natija2);
+
+
+
+// 11 masala ??????????????????????????????????
+
+// let n = 147;
+// let natija = 0;
+
+// for (let i = 0; i <= n; i++) {
+//   if ((i % 10) == ) {
+//     natija += i
+//   }
+// }
+// console.log(natija);
